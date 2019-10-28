836d1068b57842159a851213f850571b8e6f4523
aima-z <aimkan86@gmail.com>
2019-10-28 18:52:43 -0500

    HomeWork finished and revised. All working good

--- src/test/java/com/cbt/tests/NavigationTests.java
+++ src/test/java/com/cbt/tests/NavigationTests.java
@@ -1,12 +1,27 @@
 package com.cbt.tests;
 
 import com.cbt.utilities.BrowserFactory;
+import com.cbt.utilities.BrowserUtils;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 public class NavigationTests {
 
     public static void main(String[] args) {
+
+
+       WebDriver driver3 = BrowserFactory.getDriver("edge");
+        driver3.get("http://facebook.com");
+        String actual3 = driver3.getCurrentUrl();
+        String expected3 = "http://facebook.com";
+
+        if (actual3.equals(expected3)) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+            System.out.println("actual: " + actual3);
+            System.out.println("expected: " + expected3);
+        }
+       driver3.close();
 
         WebDriver driver = BrowserFactory.getDriver("chrome");
         driver.get("http://facebook.com");
@@ -23,34 +38,39 @@ public class NavigationTests {
         driver.close();
 
 
+        WebDriver driver1 = BrowserFactory.getDriver("firefox");
+        driver1.get("http://facebook.com");
+        String actual1 = driver1.getCurrentUrl();
+        String expected1 = "http://facebook.com";
 
-    WebDriver driver1 = BrowserFactory.getDriver("firefox");
-    driver1.get("http://facebook.com");
-    String actual1 = driver1.getCurrentUrl();
-    String expected1 = "http://facebook.com";
+        if (actual1.equals(expected1)) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+            System.out.println("actual: " + actual1);
+            System.out.println("expected: " + expected1);
+        }
+        driver1.close();
 
-    if(actual1.equals(expected1)){
-        System.out.println("pass");
-    }else{
-        System.out.println("fail");
-        System.out.println("actual: " + actual1);
-        System.out.println("expected: " + expected1);
+
+        WebDriver driver2 = BrowserFactory.getDriver("safari");
+        BrowserUtils.wait(3);
+        driver2.get("http://facebook.com");
+        BrowserUtils.wait(1);
+        String actual2 = driver2.getCurrentUrl();
+        String expected2 = "http://facebook.com";
+
+        if (actual2.equals(expected2)) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+            System.out.println("actual: " + actual2);
+            System.out.println("expected: " + expected2);
+        }
+        driver2.close();
+
+
+
     }
-    driver1.close();
 
-
-            WebDriver driver2=new SafariDriver();
-            driver2.get("http://facebook.com");
-            String actual2=driver2.getCurrentUrl();
-            String expected2="http://facebook.com";
-
-            if(actual2.equals(expected2)){
-                System.out.println("pass");
-            }else {
-                System.out.println("fail");
-                System.out.println("actual: "+actual2);
-                System.out.println("expected: "+expected2);
-            }
-driver2.close();
-        }
-        }
+}
--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -2,31 +2,73 @@ package com.cbt.utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-import static io.github.bonigarcia.wdm.WebDriverManager.*;
+import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+import static io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver;
 
 public class BrowserFactory {
-    //we going to create a method that will return webdriver object
-    //this method will take one parameter -String browser
-    //based on the value of the browser parameter
-    //method will return corresponded webdriver object
-    //if browser=chrome, then return chromedriver object
 
-    public static WebDriver getDriver(String browser){
-        if(browser.equals("chrome")){
-            chromedriver().setup();
-            return new ChromeDriver();
-        }else if(browser.equals("firefox")){
-            firefoxdriver().setup();
-            return new FirefoxDriver();
-        }else if(browser.equals("safari")){
-            WebDriver driver=new SafariDriver();
-            return new SafariDriver();
-        }
+
+    public static WebDriver getDriver(String browser) {
+
+        System.out.println(System.getProperty("os.name"));
+
+        if (System.getProperty("os.name").contains("Mac")) {
+
+            if (browser.equals("edge")) {
+                return null;
+            }
+            if (browser.equals("chrome")) {
+                chromedriver().setup();
+                return new ChromeDriver();
+            } else if (browser.equals("firefox")) {
+                firefoxdriver().setup();
+                return new FirefoxDriver();
+            } else if (browser.equals("internet explorer")) {
+                WebDriver driver = new InternetExplorerDriver();
+                return new InternetExplorerDriver();
+            } else if (browser.equals("opera")) {
+                WebDriver driver = new OperaDriver();
+                return new OperaDriver();
+            } else if (browser.equals("safari")) {
+                WebDriver driver = new SafariDriver();
+                return new SafariDriver();
+            }
+
+        } else if (System.getProperty("os.name").contains("Windows")) {
+            if (browser.equals("safari")) {
+                return null;
+            }
+            if (browser.equals("chrome")) {
+                chromedriver().setup();
+                return new ChromeDriver();
+            } else if (browser.equals("firefox")) {
+                firefoxdriver().setup();
+                return new FirefoxDriver();
+            } else if (browser.equals("edge")) {
+                WebDriver driver = new EdgeDriver();
+                return new EdgeDriver();
+            } else if (browser.equals("internet explorer")) {
+                WebDriver driver = new InternetExplorerDriver();
+                return new InternetExplorerDriver();
+            } else if (browser.equals("opera")) {
+                WebDriver driver = new OperaDriver();
+                return new OperaDriver();
+            }
+            }
+
         return null;
     }
-
-
 }
+//we going to create a method that will return webdriver object
+//this method will take one parameter -String browser
+//based on the value of the browser parameter
+//method will return corresponded webdriver object
+//if browser=chrome, then return chromedriver object
+
+
